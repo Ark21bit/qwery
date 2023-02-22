a17cc53c97d3f55b2a3aa46a70837715e091ced1
Ark21bit <webleif@vk.com>
2023-02-22 20:43:07 +0300

    адаптация

--- assets/scripts/swiper.js
+++ assets/scripts/swiper.js
@@ -1,10 +1,10 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters    
-    loop: true,     
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },  
-  });
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".next",
+    prevEl: ".prev",
+  },
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # Верстка макета
+
 Источник макета телеграм-канал [FIGMA | Макеты для верстки](https://www.figma.com/file/G75r4esXzrWtkPq1Yyhjrk/QWERy-landing)
